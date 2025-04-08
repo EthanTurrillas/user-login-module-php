bf15f81f3470f0683ec822392e85853c8a175f31
EthanTurrillas <ethanturrillas8@gmail.com>
2025-04-08 08:35:38 +0200

    [] - Utilizacion de mockery en los primeros Tests

--- src/Application/UserLoginService.php
+++ src/Application/UserLoginService.php
@@ -2,13 +2,36 @@
 
 namespace UserLoginService\Application;
 
+use UserLoginService\Domain\User;
+use UserLoginService\Infrastructure\FacebookSessionManager;
+
 class UserLoginService
 {
     private array $loggedUsers = [];
 
-    public function manualLogin(): string
+    public function __construct(private FacebookSessionManager $facebookSessionManager)
     {
-        return "user logged";
     }
 
+    public function manualLogin(User $user): string
+    {
+        if (in_array($user->getUserName(), $this->loggedUsers)) {
+            throw new \Exception("User already logged in");
+        }
+        $this->loggedUsers[] = $user->getUserName();
+        return "Login successful";
+    }
+
+    /**
+     * @return array
+     */
+    public function getLoggedUsers(): array
+    {
+        return $this->loggedUsers;
+    }
+
+    public function getExternalSession()
+    {
+        return $this->facebookSessionManager->getSessions();
+    }
 }
--- tests/Application/UserLoginServiceTest.php
+++ tests/Application/UserLoginServiceTest.php
@@ -4,18 +4,57 @@ declare(strict_types=1);
 
 namespace UserLoginService\Tests\Application;
 
+use Mockery;
 use PHPUnit\Framework\TestCase;
 use UserLoginService\Application\UserLoginService;
+use UserLoginService\Domain\User;
+use UserLoginService\Infrastructure\FacebookSessionManager;
 
 final class UserLoginServiceTest extends TestCase
 {
     /**
      * @test
      */
+    public function userAlreadyLoggedIn()
+    {
+        $user = new User('Asier');
+        $facebookSessionManager = Mockery::mock(FacebookSessionManager::class);
+        $userLoginService = new UserLoginService($facebookSessionManager);
+
+        $this->expectExceptionMessage("User already logged in");
+
+        $userLoginService->manualLogin($user);
+        $userLoginService->manualLogin($user);
+    }
+
+    /**
+     * @test
+     */
     public function userIsLoggedIn()
     {
-        $userLoginService = new UserLoginService();
+        $user = new User("Asier");
+        $facebookSessionManager = Mockery::mock(FacebookSessionManager::class);
+        $userLoginService = new UserLoginService($facebookSessionManager);
 
-        $this->assertEquals("user logged", $userLoginService->manualLogin());
+        $result = $userLoginService->manualLogin($user);
+
+        $this->assertEquals("Login successful", $result);
+        $this->assertContains("Asier", $userLoginService->getLoggedUsers());
+    }
+
+
+    /**
+     * @test
+     */
+    public function testGetNumberOfSessions()
+    {
+        $facebookSessionManager = Mockery::mock(FacebookSessionManager::class);
+        $facebookSessionManager->shouldReceive('getSessions')
+            ->once()
+            ->andReturn(4);
+
+        $userLoginService = new UserLoginService($facebookSessionManager);
+
+        $this->assertEquals(4, $userLoginService->getExternalSession());
     }
 }
